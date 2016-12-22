d397ca44eaac7753f1342c8c582584069eec2068
bcruddy <briancruddy@gmail.com>
2016-12-22 18:58:10 -0500

    cleanup, add some goodies

--- footer.php
+++ footer.php
@@ -1,24 +1,5 @@
-<?php
-
-?>
-
-<!DOCTYPE html>
-<html>
-<head><meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
-<title>FUCK THE JETS</title>
-
-</head>
-	<style>
-		.contribute {
-			position: relative;
-			font-family: "Arial", Gadget, sans-serif;
-		}
-	</style>
-	<body>
-		
-		<div class="contribute">
-                        <h3 style="position: fixed; bottom: 0; width:100%; text-align: center"><span style="font-size:12px;">Contribute to this website <a href="https://github.com/FuckTheJets/fuckthejets">here</a></span><h3>
-		</div>
-		
-	</body>
-</html>
+<div class="contribute">
+    <a href="https://github.com/FuckTheJets/fuckthejets">
+        <h3>Contribute to this website<h3>
+    </a>
+</div>
--- index.php
+++ index.php
@@ -1,17 +1,15 @@
-<?php
-?>
-
-<!doctype html>
-
+<!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>FUCK THE JETS</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="/css/style.css">
 </head>
-<body style="text-align:center;"> <h1 style="font-size:180px;font-family:Helvetica,Arial,sans-serif;">FUCK THE JETS</h1>
-<iframe width="640" height="360" src="https://www.youtube.com/embed/rAp57G1hLn0?autoplay=1" frameborder="0" allowfullscreen></iframe>
-	
+<body>
+    <h1 class="heading">FUCK THE JETS</h1>
+    <iframe width="640" height="360" src="https://www.youtube.com/embed/rAp57G1hLn0?autoplay=1" frameborder="0" allowfullscreen></iframe>
+    <?php include 'footer.php'; ?>
+    <script src="/js/timer.js"></script>
 </body>
 </html>
-<?php include 'footer.php'; ?>
