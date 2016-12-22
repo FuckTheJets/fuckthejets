ce98a3cebeccb78988b5e86b49fe28ed7a84fd6b
FuckTheJets <FuckTheJets@users.noreply.github.com>
2016-12-22 14:41:46 -0800

    Revert "Added some basic layout components"

--- index.php
+++ index.php
@@ -1,26 +1,17 @@
 <?php
 ?>
+
 <!doctype html>
+
 <html lang="en">
 <head>
 	<meta charset="utf-8">
 	<title>FUCK THE JETS</title>
-	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
-	<link rel="stylesheet" type="text/css" href="/css/style.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
-<body>
-	<nav class="nav">the jets fucking suck</nav>
-	<main class="main-content">
-		<h2 class="main-title">FUCK THE JETS</h2>
-		<div class="h-scroll">
-			<div class="h-scroll-content fade-in">
-				<iframe width="640" height="360" src="https://www.youtube.com/embed/rAp57G1hLn0?autoplay=1" frameborder="0" allowfullscreen></iframe>
-				<img src="http://i.imgur.com/BLL33.gif" alt="">
-			</div>
-		</div>
-
-		<img class="fade-in" src="http://i.imgur.com/9ZiK73r.png" alt="">
-	</main>
-	<?php include 'footer.php'; ?>
+<body style="text-align:center;"> <h1 style="font-size:180px;font-family:Helvetica,Arial,sans-serif;">FUCK THE JETS</h1>
+<iframe width="640" height="360" src="https://www.youtube.com/embed/rAp57G1hLn0?autoplay=1" frameborder="0" allowfullscreen></iframe>
+	
 </body>
 </html>
+<?php include 'footer.php'; ?>
