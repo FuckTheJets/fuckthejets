2a59279a0768c1d54ce6ef5d831fead38ed4fd04
FuckTheJets <FuckTheJets@users.noreply.github.com>
2016-12-22 14:09:02 -0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,4 +1,29 @@
 <?php
 
-
 ?>
+
+<!DOCTYPE html>
+<html>
+<head><meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+<title>FUCK THE JETS</title>
+
+</head>
+  <style>
+		.contribute {
+			position: absolute;
+			top: 50%;
+			left: 50%;
+			-moz-transform: translateX(-50%) translateY(-50%);
+			-webkit-transform: translateX(-50%) translateY(-50%);
+			transform: translateX(-50%) translateY(-50%);
+			font-family: "Arial Black", Gadget, sans-serif;
+		}
+	</style>
+	<body>
+		
+<div class="contribute">
+  <h3 style="position: fixed; bottom: 0; width:100%; text-align: center"><span style="font-size:12px;">Contribute to this website<a href="https://github.com/FuckTheJets/fuckthejets">here</a></span><h3>
+</div>
+		
+	</body>
+</html>
