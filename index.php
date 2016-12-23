3852bbf10e48021a67f1021aba6fdf8dadf5ba88
FuckTheJets <FuckTheJets@users.noreply.github.com>
2016-12-22 21:56:53 -0800

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 	<title>FUCK THE JETS</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
-<body style="text-align:center;"> <h1 style="font-size:180px;font-family:Helvetica,Arial,sans-serif;">FUCK THE JETS</h1>
+<body style="text-align:center;"> <h1 style="font-size:160px;font-family:Helvetica,Arial,sans-serif;">FUCK THE JETS</h1>
 <iframe width="640" height="360" src="https://www.youtube.com/embed/rAp57G1hLn0?autoplay=1" frameborder="0" allowfullscreen></iframe>
 <div class="tc_div_56318" style="margin-top:50px;width:650px;height:162.50px"><a title="Days Since Last Jets Title" href="//www.tickcounter.com/widget/countup/-30549600000/america-new_york/d/FFFFFF0C371D000000FF0000/650/C0C0C01/Days_Since_Jets_Last_Title">Days Since Jets Last Title</a><a title="Count Up" href="https://www.tickcounter.com/countup">Count Up</a></div><script type="text/javascript">(function(){ var s=document.createElement('script');s.src="//www.tickcounter.com/loader.js";s.async='async';s.onload=function() { tc_widget_loader('tc_div_56318', 'CountUp', 650, ["-30549600000","america-new_york","d","FFFFFF0C371D0C371DFF0000","650","C0C0C01","Days Since Jets Last Title"]);};s.onreadystatechange=s.onload;var head=document.getElementsByTagName('head')[0];head.appendChild(s);}());</script>
 </body>
